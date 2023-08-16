9ef0321e63a7ee1e2a6469d976314d2749ccaf06
ShafiaAnsar <shafiaansar142@gmail.com>
2023-08-16 09:29:49 +0500

    Lets build node app

---
+++ app.js
@@ -0,0 +1,17 @@
+require ('dotenv').config()
+
+const express = require('express')
+const expresLayouts = require('express-ejs-layouts')
+
+const app = express()
+const port = 5000 || process.env.port
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+//Static Files
+app.use(express.static('public'))
+
+//Templating Engines
+app.use(expresLayouts)
+app.set('layout','./layouts/main')
+app.set('view engine','ejs')
