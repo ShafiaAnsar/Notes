42eb2df69bf11078fea5c27e9de5b40f72e12511
ShafiaAnsar <shafiaansar142@gmail.com>
2023-08-19 12:11:30 +0500

    Dashboard

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -1,23 +1,14 @@
 
 // Get Dashboard..
 
-exports.homepage = async (req, res) =>{
+exports.dashboard = async (req, res) =>{
     const locals = {
         title :'Dashboard',
         description :'Free NodeJs Notes App'
     }
-    res.render('dashboard',{
+    res.render('dashboard/index',{
         locals,
         layout:'../views/layouts/dashboard'
     })
 }
 
-// Get About
-
-exports.about = async (req, res) =>{
-    const locals = {
-        title :' About - NodeJs Notes',
-        description :'Free NodeJs Notes App'
-    }
-    res.render('about',locals)
-}
