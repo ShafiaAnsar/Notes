e157e093fd812717c5d9cbd45ae5a3541c4609d1
ShafiaAnsar <shafiaansar142@gmail.com>
2023-08-20 11:58:41 +0500

    Google Authentication

--- app.js
+++ app.js
@@ -6,8 +6,22 @@ const connectdb = require('./server/config/db')
 const session = require ('express-session')
 const passport = require ('passport')
 const MongoStore = require("connect-mongo")
+
+
 const app = express()
 const port = 5000 || process.env.port
+
+app.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: false
+}));
+
+
+
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
@@ -20,6 +34,7 @@ app.set('layout','./layouts/main')
 app.set('view engine','ejs')
 
 //Routes
+app.use('/', require('./server/routes/auth'));
 app.use('/', require('./server/routes/index'));
 app.use('/', require('./server/routes/dashboard'));
 // Handle 404
---
+++ server/routes/auth.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+const passport = require('passport')
+
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+module.exports = router
