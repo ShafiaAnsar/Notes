ff9337c2770b7482e11fb266c340256498ee2257
ShafiaAnsar <shafiaansar142@gmail.com>
2023-08-21 10:23:28 +0500

    Lets build dashboard page

---
+++ server/middleware/checkAuth.js
@@ -0,0 +1,8 @@
+exports.isLoggedIn= (req,res,next) =>{
+if (req.user){
+    next()
+}
+else{
+    return res.status(401).send('Access Denied')
+}
+}
--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const  {isLoggedIn} = require('../middleware/checkAuth')
 const dashboardController = require('../controllers/dashboardController')
 
 
 //Dashboard Routes
-router.get('/dashboard',dashboardController.dashboard)
+router.get('/dashboard',isLoggedIn,dashboardController.dashboard)
 
 module.exports= router
