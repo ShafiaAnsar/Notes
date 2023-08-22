afb30c2cb40d7f0452c899c5f05104f761ea8e06
ShafiaAnsar <shafiaansar142@gmail.com>
2023-08-22 11:00:30 +0500

    Pagination

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -3,22 +3,49 @@ const mongoose = require('mongoose')
 // Get Dashboard..
 
 exports.dashboard = async (req, res) =>{
-
+   let perPage = 12 
+   let page = req.query.page || 1
     const locals = {
             title :'Dashboard',
             description :'Free NodeJs Notes App'
         }
-
-
-
      try {
-        const notes = await Note.find({})
-        res.render('dashboard/index',{
-            userName:req.user.firstName,
-            locals,
-            notes,
-            layout:'../views/layouts/dashboard'
-        })
+      Note.aggregate([
+         {
+            $sort:{
+               createdAt:-1
+            }
+         },
+         {
+            $match:{
+               user:mongoose.Types.ObjectId(req.user.id)
+            }
+         },
+         {
+            $project:{
+               title:{$substr:['$title',0,30]},
+               body:{$substr:['$body',0,100]}
+            }
+         }
+      ]).skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec(function (err,notes){
+         Note.count().exec(function(err,count){
+            if (err){
+               return next(err)
+            }
+            // const notes = await Note.find({})
+            res.render('dashboard/index',{
+                userName:req.user.firstName,
+                locals,
+                notes,
+                layout:'../views/layouts/dashboard',
+                pages:Math.ceil(count/perPage)
+            })
+         })
+      })
+
+       
         
      } catch (error) {
         console.log(error)
