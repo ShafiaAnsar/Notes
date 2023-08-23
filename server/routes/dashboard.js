61bb49543f72498dd3e0037c4b185c5e990ccb53
ShafiaAnsar <shafiaansar142@gmail.com>
2023-08-23 10:47:15 +0500

    Search Note

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -11,5 +11,7 @@ router.put('/dashboard/item/:id', isLoggedIn, dashboardController.dashboardUpdat
 router.delete('/dashboard/item-delete/:id', isLoggedIn, dashboardController.dashboardDeleteNote);
 router.get('/dashboard/add', isLoggedIn, dashboardController.dashboardAddNote);
 router.post('/dashboard/add', isLoggedIn, dashboardController.dashboardAddNoteSubmit);
+router.get('/dashboard/search', isLoggedIn, dashboardController.dashboardSearch);
+router.post('/dashboard/search', isLoggedIn, dashboardController.dashboardSearchSubmit);
 
 module.exports= router
